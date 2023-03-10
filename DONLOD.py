c03de6b1afd35a4f19c48a900f15be93b83e09bc
Tanmay Kule <tanmaythegreat17@gmail.com>
2023-03-10 16:24:50 +0530

    fixedthe ye

--- DONLOD.py
+++ DONLOD.py
@@ -1,19 +1,33 @@
 import requests
 import time
 import json
+from tqdm import tqdm
+
 url = "https://kitsu.moe/api/b/"
 d_url = "https://kitsu.moe/d/"
 # id = input("Enter beatmap id: ").split()
 
-def download(file_path, bid):
+def download(file_path, bid, chunk_size=1024):
     count = 0
     for _ in bid:
         response = requests.get(url+bid[count])
         setID = str(json.loads(response.content)["ParentSetID"])
         response2 = requests.get(d_url+setID)
-        open(f"{file_path}/{setID}.osz", "wb").write(response2.content)
+        total = int(response2.headers.get('content-length', 0))
+        #open(f"{file_path}/{setID}.osz", "wb").write(response2.content)
+        with open(f"{file_path}/{setID}.osz", "wb") as file, tqdm(desc=f"{file_path}/{setID}.osz",
+        total=total,
+        unit='iB',
+        unit_scale=True,
+        unit_divisor=1024,) as bar:
+            for data in response2.iter_content(chunk_size=chunk_size):
+                size = file.write(data)
+                bar.update(size)
+                print(bar)
+                
         print(f"done donwloading {setID}")
         count+=1
+
 
     
 
--- GOoey.py
+++ GOoey.py
@@ -1,7 +1,8 @@
 import customtkinter
-import tkinter
 from DONLOD import *
 import shutil
+import threading
+from PIL import Image
 
 DOWNLOAD_DEST_PATH = "maps"
 ZIPFILE_PATH = "pool"
@@ -10,38 +11,33 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
 
 root = customtkinter.CTk()
-root.geometry("800x600")
+root.geometry("1024x768")
 
 frameCnt = 104
-frames = [tkinter.PhotoImage(file='ramez_trans.gif',format = 'gif -index %i' %(i)) for i in range(frameCnt)]
+#frames = customtkinter.CTkImage(Image.open('rameez.png'),size=(80,50))
 
-def update(ind):
-    global frameCnt
-    global label
-    frame = frames[ind]
-    ind += 1
-    if ind == frameCnt:
-        ind = 0
-    label.configure(image=frame)
-    root.after(100, update, ind)
-
-def remove_image():
-    global label
-    label.configure(image=None)
 
 def donload():
+    # button.configure(text="",image=frames)
+   # rameez = customtkinter.CTkLabel(master=frame, image=frames, text="",height=80,width=50)
+   # rameez.pack(pady=12,padx=10)
+    global donload_thread
     bid = entry1.get("1.0",'end-1c').split()
-    print(bid)
-    root.after(0, update, 0)
-    download(DOWNLOAD_DEST_PATH, bid)
-    remove_image()
+
+    prog = customtkinter.CTkProgressBar(master=frame)
+    prog.pack(pady=12, padx=10)
+    download_thread = threading.Thread(target=lambda:download(DOWNLOAD_DEST_PATH, bid))
+    download_thread.start()
+    
 
     print("done donlod")
-    # del loading_screen
+    donload_thread = threading.Thread(target=donload)
 
 def zip():
-    shutil.make_archive(ZIPFILE_PATH, 'zip', "maps/")
+    zip_thread = threading.Thread(target=lambda:shutil.make_archive(ZIPFILE_PATH, 'zip', "maps/"))
+    zip_thread.start()
 
+zip_thread = threading.Thread(target=zip)
 
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
@@ -52,7 +48,7 @@ label.pack(pady=12, padx=10)
 entry1 = customtkinter.CTkTextbox(master=frame, width=454, height=200)
 entry1.pack(pady=12, padx=10)
 
-button = customtkinter.CTkButton(master=frame, text="DONLOD", command=donload)
+button = customtkinter.CTkButton(master=frame, text="DONLOD", command=donload, height=100,width=120)
 button.pack(pady=12, padx=10)
 
 button2 = customtkinter.CTkButton(master=frame, text="ZIP pls", command=zip)
